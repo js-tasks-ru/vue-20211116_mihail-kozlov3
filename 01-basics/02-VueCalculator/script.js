20fe3e9e76953280811a67d0ff3dac7ded70ded3
khf178@khf.ru <khf178@khf.ru>
2021-11-26 12:15:41 +1000

    calculator in progress

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+//import { Vue } from './vendor/vue.esm-browser.js';
+
+//Vue.config.devtools = true
 
 // Создайте Vue приложение
+const app = createApp({
+    data() {
+        return {
+            firstVal: 0
+            , secondVal: 0
+            , result: 0
+        };
+    }
+    , methods: {
+        add() {
+            this.result = this.firstVal + this.secondVal;
+            console.log(this.firstVal, this.secondVal, this.result);
+        }
+        , sub() {
+            this.result = this.firstVal - this.secondVal;
+        }
+        , mul() {
+            this.result = this.firstVal * this.secondVal;
+        }
+        , div() {
+            this.result = this.firstVal / this.secondVal;
+        }
+    }
+}).mount("#app");
